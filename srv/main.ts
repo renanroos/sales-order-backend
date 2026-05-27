4b7c75d2e8414ebe5519d318c3b93d4785d3d31a
renan.roos <renan-roos@hotmail.com>
2026-05-27 11:59:07 -0300

    feat: add products validation

--- srv/main.ts
+++ srv/main.ts
@@ -1,22 +1,23 @@
 import cds, { EventHandler, ResultsHandler, Service } from '@sap/cds';
-import { Customers } from '@models/sales';
+import { Customers, SalesOrderItem, SalesOrderItems, Products } from '@models/sales';
 
 export default (service: Service) => {
     service.after('READ', 'Customers', (results: Customers) => {
         results.forEach(customer => {
             if (!customer.email?.includes('@')) {
                 customer.email = '${customer.email}@gmail.com';
-            }   
+            }
         });
     });
 
     service.before('CREATE', 'SalesOrderHeaders', async (req) => {
         const params = req.data;
-        if(!params.customer_id) {
+        const items: SalesOrderItems = params.items;
+        if (!params.customer_id) {
             return req.reject(400, 'Customer Inválido');
         }
 
-        if(!params.items || params.items.length === 0) {
+        if (!params.items || params.items.length === 0) {
             return req.reject(400, 'A ordem de venda deve conter pelo menos um item');
         }
 
@@ -24,6 +25,24 @@ export default (service: Service) => {
         const customer = await cds.run(customerQuery);
         if (!customer) {
             return req.reject(404, 'Customer não encontrado');
+        }
+
+        const productsIds: string[] = params.items.map((item: SalesOrderItem) => item.product_id);
+        const productsQuery = SELECT.from('sales.Products').where({ id: productsIds });
+        const products: Products = await cds.run(productsQuery);
+        // const dbProducts = products.map((product) => product.id);
+        for (const item of items) {
+            const dbProduct = products.find(product => product.id === item.product_id);
+            if (!dbProduct) {
+                return req.reject(404, `Produto com id ${item.product_id} não encontrado`);
+            }
+
+            // if(!productsIds.every(productId  => dbProducts.includes(productId)) ) {
+            //     return req.reject(404, 'Produto não encontrado');
+            // }
+            if (dbProduct.stock === 0) {
+                return req.reject(400, `Produto ${dbProduct.name} sem estoque disponível`);
+            }
         }
     });
 }
