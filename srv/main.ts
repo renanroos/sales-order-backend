8506b01a0cf287eef411ac8968093c89e1be28d5
renan.roos <renan-roos@hotmail.com>
2026-05-27 11:19:08 -0300

    feat: add before event in main.ts

--- srv/main.ts
+++ srv/main.ts
@@ -1,4 +1,4 @@
-import { ResultsHandler, Service } from '@sap/cds';
+import cds, { EventHandler, ResultsHandler, Service } from '@sap/cds';
 import { Customers } from '@models/sales';
 
 export default (service: Service) => {
@@ -8,5 +8,22 @@ export default (service: Service) => {
                 customer.email = '${customer.email}@gmail.com';
             }   
         });
+    });
+
+    service.before('CREATE', 'SalesOrderHeaders', async (req) => {
+        const params = req.data;
+        if(!params.customer_id) {
+            return req.reject(400, 'Customer Inválido');
+        }
+
+        if(!params.items || params.items.length === 0) {
+            return req.reject(400, 'A ordem de venda deve conter pelo menos um item');
+        }
+
+        const customerQuery = SELECT.one.from('sales.Customers').where({ id: params.customer_id });
+        const customer = await cds.run(customerQuery);
+        if (!customer) {
+            return req.reject(404, 'Customer não encontrado');
+        }
     });
 }
